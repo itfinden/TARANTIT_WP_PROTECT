87a3da1f702845e83e1d41e96c117466c26d4e9f
Man Of Orion <cm@itfinden.com>
2019-11-06 14:41:00 -0300

    Create T_IT_functions

---
+++ libs/tarantit_functions.php
@@ -0,0 +1,36 @@
+<?php
+namespace TarantIT;
+
+/**
+ *
+ */
+class TIT_functions {
+
+	/**
+	 * Remove the transients set when verifying the restrictions.
+	 *
+	 * @return void
+	 */
+	public static function reset_plugin_transients() {
+		global $wpdb;
+		// Remove all the transients records in one query.
+		$tmp_query = $wpdb->prepare(
+			' DELETE FROM ' . $wpdb->options . ' WHERE option_name LIKE %s OR option_name LIKE %s ',
+			$wpdb->esc_like('_transient_geo-country-code') . '%',
+			$wpdb->esc_like('_transient_timeout_geo-country-code') . '%'
+		);
+		$wpdb->query($tmp_query); // WPCS: Unprepared SQL OK.
+
+		if (is_multisite()) {
+			// Attempt to flush transient also on multisite.
+			$tmp_query = $wpdb->prepare(
+				' DELETE FROM ' . $wpdb->sitemeta . ' WHERE meta_key LIKE %s OR option_name LIKE %s ',
+				$wpdb->esc_like('_transient_geo-country-code') . '%',
+				$wpdb->esc_like('_transient_timeout_geo-country-code') . '%'
+			);
+			$wpdb->query($tmp_query); // WPCS: Unprepared SQL OK.
+		}
+	}
+
+}
+?>
